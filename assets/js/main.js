0ffd0362f65b04128896de01c21d82267a02751a
Barento Muhedin <awesomemoi@hotmail.com>
2025-03-27 19:09:36 -0400

    Header animation added

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,3 +30,18 @@
 		$('.scrolly').scrolly();
 
 })(jQuery);
+
+//Fade in
+const observer = new IntersectionObserver((entries) => {
+	entries.forEach((entry) => {
+		if (entry.isIntersecting) {
+			entry.target.classList.add('show');
+		} else {
+			entry.target.classList.remove('show');
+		}
+	});
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+
+hiddenElements.forEach((el) => observer.observe(el));
